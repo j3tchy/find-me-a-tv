1777bfe6f6e0835dd2c5f82984959f3f157c2eb8
J <test@example.com>
2020-11-04 23:19:26 +0000

    initial commit

---
+++ scraper.js
@@ -0,0 +1,27 @@
+const fetch = require("node-fetch");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const websites = require("./data.json");
+
+async function scrapeForTV(url, element) {
+  try {
+      const response = await fetch(url);
+      const text = await response.text();
+      const dom = await new JSDOM(text);
+      const currentPrice = dom.window.document.querySelector(element).textContent;
+
+      const stripOutPoundSign = currentPrice.replace(/£/gm, "");
+      const price = Number(stripOutPoundSign.split('.')[0]);
+
+      if (price < 799) {
+        console.log(url, price);
+      }
+
+      console.log(url, price);
+    } catch (e) {
+      console.error(e);
+    }
+}
+
+websites.forEach(({ url, element }) => scrapeForTV(url, element));
